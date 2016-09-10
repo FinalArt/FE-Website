aded6df36642bd696ce22a3d940fad0f92e8ec3c
FinalArt <finalart.mail@gmail.com>
2016-09-10 13:31:50 +0200

    - Basic Pokedex reading a JSON file located in Data directory - Show a pokemon (or not) with an attribute - Create fragments for templates - Use navbar to go to Pokedex

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,26 @@
 <?php
-// Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+// Homepage of the website
+$app->get('/', function ($request, $response, $args) {
     $this->logger->info("Slim-Skeleton '/' route");
-    return $this->view->render($response, 'index.html', $args);
+    return $this->view->render($response, 'index.html', [
+        "navbarCurrentItem" => "index"
+    ]);
+});
+
+// Pokedex containing all pokemon showable
+$app->get('/pokedex', function ($request, $response, $args) {
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    $fileName = __DIR__ . '/../data/pokemons.json';
+    $pokemons = [];
+    if (file_exists($fileName)) {
+        $json = file_get_contents($fileName);
+        $pokemons = json_decode($json, true);
+    }
+
+    return $this->view->render($response, "pokedex.html", [
+        "navbarCurrentItem" => "pokedex",
+        "pokemons" => $pokemons
+    ]);
 });
