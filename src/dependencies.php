2f1908a7a202f41657cdb7d5807ca094ce2a7bb4
FinalArt <finalart.mail@gmail.com>
2016-09-23 14:19:05 +0200

    Extract color filter from vendor directory

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,7 +3,7 @@
 
 $container = $app->getContainer();
 
-// view renderer
+// View renderer
 $container['view'] = function ($c) {
     $settings = $c->get('settings')['renderer'];
     $view = new \Slim\Views\Twig($settings['template_path'], [
@@ -13,10 +13,11 @@ $container['view'] = function ($c) {
         $c['router'],
         $c['request']->getUri()
     ));
+    $view->getEnvironment()->addFilter(new Twig_SimpleFilter('typeToColor', array('Filter', 'typeToColor')));
     return $view;
 };
 
-// monolog
+// Monolog
 $container['logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
     $logger = new Monolog\Logger($settings['name']);
@@ -24,3 +25,81 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+/**
+ * Provide tools for Twig's templates
+ */
+class Filter
+{
+
+    /**
+     * Associate a pokemon type to a Semantic-UI color
+     *
+     * @param $type
+     * @return string
+     */
+    public function typeToColor($type)
+    {
+        $color = "";
+        switch ($type) {
+            case "Normal" :
+                $color = "grey";
+                break;
+            case "Fire" :
+                $color = "red";
+                break;
+            case "Water" :
+                $color = "blue";
+                break;
+            case "Electric" :
+                $color = "yellow";
+                break;
+            case "Grass" :
+                $color = "green";
+                break;
+            case "Ice" :
+                $color = "teal";
+                break;
+            case "Fighting" :
+                $color = "orange";
+                break;
+            case "Poison" :
+                $color = "purple";
+                break;
+            case "Ground" :
+                $color = "brown";
+                break;
+            case "Flying" :
+                $color = ""; // Use the basic "grey" color
+                break;
+            case "Psychic" :
+                $color = "purple";
+                break;
+            case "Bug" :
+                $color = "olive";
+                break;
+            case "Rock" :
+                $color = "brown";
+                break;
+            case "Ghost" :
+                $color = "black";
+                break;
+            case "Dragon" :
+                $color = "violet";
+                break;
+            case "Dark" :
+                $color = "black";
+                break;
+            case "Steel" :
+                $color = ""; // Use the basic "grey" color
+                break;
+            case "Fairy" :
+                $color = "pink";
+                break;
+            default :
+                break;
+        }
+        return $color;
+    }
+
+}
